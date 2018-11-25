162bbdaf0ef25a35d74836f470ea85728df14856
Chang-Jin <2475477667@qq.com>
2018-11-25 21:18:23 +0800

    添加了filters 加入了基本功能

--- pages/index/index.js
+++ pages/index/index.js
@@ -4,9 +4,28 @@ const app = getApp()
 
 Page({
   data: {
-    motto: '富强 民主 文明 和谐自由 平等 公正 法治爱国 敬业 诚信 友善'
+    motto: '富强 民主 文明 和谐自由 平等 公正 法治爱国 敬业 诚信 友善',
+    buttonShow: true,
+    time: 11 * 60
   },
   onLoad: function () {},
-  getUserInfo: function(e) {
+  startClock: function (e) {
+    this.setData({
+      buttonShow: !this.data.buttonShow
+    })
+    let _interval = setInterval(() => {
+      let timeNow = this.data.time - 1
+
+      this.setData({
+        time: timeNow
+      })
+
+      if (timeNow < 1) {
+        clearInterval(_interval)
+        this.setData({
+          buttonShow: !this.data.buttonShow
+        })
+      }
+    }, 20)
   }
 })
